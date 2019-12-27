bb773686ef91214c5411b302753b2fbfc35bee6c
kitty433cute <beloliptsev.ma@perfema.com>
2019-12-27 13:01:23 +0300

    initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, {RefObject, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState(0);
+    const [result, setResult] = useState("click to know")
+    let lastElement: RefObject<HTMLDivElement> = React.createRef()
+
+    const handleInput = (a: string) => {
+        const number = Number.parseInt(a) && (Number.parseInt(a) > 0) ? Number.parseInt(a) : 0;
+        setValue(number)
+    }
+
+    const isEven = () => {
+        const color = lastElement.current && lastElement.current.lastElementChild && window.getComputedStyle(lastElement.current.lastElementChild).backgroundColor
+        console.log(color)
+        if (color && (color == "rgb(255, 0, 0)")) {
+            setResult("number is even!")
+        } else setResult("number is odd!")
+    }
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>
+                    <p>
+                        is even????
+                    </p>
+                    {result}
+                </div>
+                <input
+                    value={value}
+                    placeholder={"enteer your number"}
+                    type={"number"}
+                    onChange={a => handleInput(a.currentTarget.value)}
+                />
+                <button onClick={e => isEven()}>
+                    click to know
+                </button>
+                <div className={"values"} ref={lastElement}>
+                    {
+                        new Array(value).fill("").map((a, i) => {
+                            return <div key={`${i}`} id={`${i + 1}`} className="countable">{i + 1}</div>;
+                        })
+                    }
+                </div>
+                {value}
+            </header>
+
+        </div>
+    );
 }
 
 export default App;
